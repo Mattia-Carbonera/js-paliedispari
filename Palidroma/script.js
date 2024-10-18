24151306d313029dc86d7112522f59dddb8c01ba
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-10-18 13:41:56 +0200

    add string request

---
+++ Palidroma/script.js
@@ -0,0 +1,11 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// chiedo all'utente di inserire una parola
+const userWord = prompt("Digiti una parola");
+console.log(`La tua parola è: "${userWord}"`);
+
+// controllo che la parola sia valida
+// verifico se la parola letta al contrario è uguale
+// restituisco il risultato all'utente
