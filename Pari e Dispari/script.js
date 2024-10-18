0fc196fcd14d4727e1eb1647728462b99651c8b0
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-10-18 14:28:48 +0200

    add user data request

---
+++ Pari e Dispari/script.js
@@ -0,0 +1,19 @@
+// Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri
+// Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+// Dichiariamo chi ha vinto.
+
+// chiedo all'utente se pari o dispari
+const userEvenOdd = prompt('Inserisca se "pari" o "dispari"').toString();
+const userNumb = parseInt(prompt("Inserisca un numero da 1 a 5"));
+// console.log(userEvenOdd);
+// console.log(userNumb);
+
+// controllo i dati inseriti dall'utente
+// chiedo all'utente un numero da 1 a 5
+// genero un numero randomico per i computer
+// sommo i numeri
+// stabilisco pari o dispari
+// restituisco il vincitore
