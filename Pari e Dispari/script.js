ea5f3a51ec155a42c1b20e4b1c958fccf6577753
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-10-18 14:50:18 +0200

    add randomaizer function

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -5,15 +5,27 @@
 // Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 // Dichiariamo chi ha vinto.
 
-// chiedo all'utente se pari o dispari
+// chiedo all'utente se pari o dispari e numero da 1 a 5
+
 const userEvenOdd = prompt('Inserisca se "pari" o "dispari"').toString();
 const userNumb = parseInt(prompt("Inserisca un numero da 1 a 5"));
+const maxRandom = 5;
+
 // console.log(userEvenOdd);
 // console.log(userNumb);
 
 // controllo i dati inseriti dall'utente
-// chiedo all'utente un numero da 1 a 5
+
 // genero un numero randomico per i computer
+
+function computerNum(maxRandom) {
+  let computerRandom = Math.floor(Math.random() * maxRandom) + 1;
+
+  return computerRandom;
+}
+
+// console.log("numero generato casualmente " + computerNum(5));
+
 // sommo i numeri
 // stabilisco pari o dispari
 // restituisco il vincitore
