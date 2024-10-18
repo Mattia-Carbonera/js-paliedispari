b6e0b7438622d7b17e16b10dff7763effe141b44
Mattia-Carbonera <mattia.carbonera@gmail.com>
2024-10-18 15:08:04 +0200

    add sum function

--- Pari e Dispari/script.js
+++ Pari e Dispari/script.js
@@ -11,21 +11,31 @@ const userEvenOdd = prompt('Inserisca se "pari" o "dispari"').toString();
 const userNumb = parseInt(prompt("Inserisca un numero da 1 a 5"));
 const maxRandom = 5;
 
-// console.log(userEvenOdd);
-// console.log(userNumb);
+console.log("pari o dispari user " + userEvenOdd);
+console.log("numero user " + userNumb);
 
 // controllo i dati inseriti dall'utente
 
 // genero un numero randomico per i computer
 
 function computerNum(maxRandom) {
-  let computerRandom = Math.floor(Math.random() * maxRandom) + 1;
+  const computerRandom = Math.floor(Math.random() * maxRandom) + 1;
 
   return computerRandom;
 }
 
-// console.log("numero generato casualmente " + computerNum(5));
+const randomNumber = computerNum(maxRandom);
+console.log("numero generato casualmente " + randomNumber);
 
 // sommo i numeri
+
+function numberSum(userNum, randomicNum) {
+  const result = userNum + randomicNum;
+  return result;
+}
+
+const finalSum = numberSum(userNumb, randomNumber);
+console.log(finalSum);
+
 // stabilisco pari o dispari
 // restituisco il vincitore
